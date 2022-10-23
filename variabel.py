929b230e65c1a38e988a4583e421c226a4562356
femas <telebeduk01@gmail.com>
2022-10-22 22:31:39 -0400

    three commit

---
+++ variabel.py
@@ -0,0 +1,30 @@
+#proses memasukan data ke dalam variabel
+nama = "fems"
+#proses mencetak variabel
+print(nama)
+
+#nilai dan tipe data dalam variabel  dapat diubah
+umur = 18               #nilai awal
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+umur = "delapan belas" #nilai setelah diubah
+print(umur)             #mencetak nilai umur
+type(umur)              #mengecek tipe data umur
+
+namadepan = "femas"
+namabelakang = "anugra"
+namapanjang= namadepan + " " + namabelakang
+umur = 18
+asal = "sumbawa"
+print("Biodata\n", namapanjang, "\n", umur, "\n", asal)
+
+#contoh variabel lainya
+inivariabel = "Hallo"
+ini_juga_variabel = "Hai"
+_inivariabeljuga = "Hi"
+inivariabel222 = "hola" 
+
+panjang_ruangan = 10 
+lebar_ruangan= 20
+luas_ruangan = panjang_ruangan * lebar_ruangan
+print(luas_ruangan)
